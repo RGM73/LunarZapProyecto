0f44afbf254cf3c61c8297964e5c2cd5a10cd120
RaduGabrielMaría <rgabriel@ginzo.tech>
2023-05-30 12:52:35 +0200

    Image perfil zap

--- app/src/main/java/com/example/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/myapplication/MyAdapter.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,74 +23,107 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private List<ZAP> mZAPList;
+    private FirebaseFirestore db;
+    Context context;
 
     public MyAdapter(List<ZAP> ZAPList) {
         mZAPList = ZAPList;
+        db = FirebaseFirestore.getInstance();
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
-
-        // Inflate the custom layout
         View ZAPView = inflater.inflate(R.layout.zap, parent, false);
-
-        // Return a new holder instance
-        ViewHolder viewHolder = new ViewHolder(ZAPView);
-        return viewHolder;
-    }
-
-    public void setDataList(List<ZAP> ZAPList) {
-        mZAPList = ZAPList;
-        notifyDataSetChanged();
+        return new ViewHolder(ZAPView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        // Obtener el modelo de datos según la posición
         ZAP zap = mZAPList.get(position);
 
-        // Establecer los valores de los elementos de la vista
         holder.usernameTextView.setText(zap.getUsuario() != null ? zap.getUsuario().toString() : "");
         holder.contentTextView.setText(zap.getContenido() != null ? zap.getContenido().toString() : "");
         holder.datetimeTextView.setText(zap.getFecha() != null ? zap.getFecha().toString() : "");
+
+        Query query = db.collection("users").whereEqualTo("Username", zap.getUsuario());
+        query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot querySnapshot) {
+                if (!querySnapshot.isEmpty()) {
+                    DocumentSnapshot documentSnapshot = querySnapshot.getDocuments().get(0);
+                    String photoUrl = documentSnapshot.getString("photo");
+                    Log.d(TAG, "photoUrl: " + photoUrl);
+                    if (photoUrl != null) {
+                        Glide.with(context)
+                                .load(photoUrl)
+                                .into(holder.imageView);
+                    } else {
+                        Glide.with(holder.itemView.getContext())
+                                .load(R.drawable.profile)
+                                .into(holder.imageView);
+                    }
+                } else {
+                    // El usuario no fue encontrado en la base de datos.
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Error getting user document", e);
+            }
+        });
 
         holder.likeButton.setText("Likes: " + zap.getLikes());
         holder.dislikeButton.setText("Dislikes: " + zap.getDislikes());
     }
 
-
-
     @Override
     public int getItemCount() {
         return mZAPList.size();
     }
+    public void setDataList(List<ZAP> ZAPList) {
+        mZAPList = ZAPList;
+        notifyDataSetChanged();
+    }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
         public TextView usernameTextView;
         public TextView contentTextView;
         public TextView datetimeTextView;
-        public TextView likesTextView;
-        public TextView dislikesTextView;
         public Button likeButton;
         public Button dislikeButton;
 
         public ViewHolder(View itemView) {
             super(itemView);
-
-            usernameTextView = (TextView) itemView.findViewById(R.id.username_text);
-            contentTextView = (TextView) itemView.findViewById(R.id.content_text);
-            datetimeTextView = (TextView) itemView.findViewById(R.id.datetime_text);
-            likeButton = (Button) itemView.findViewById(R.id.like_button);
-            dislikeButton = (Button) itemView.findViewById(R.id.dislike_button);
-            // likesTextView = (TextView) itemView.findViewById(R.id.likes_text);
-            // dislikesTextView = (TextView) itemView.findViewById(R.id.dislikes_text);
+            imageView = itemView.findViewById(R.id.avatar_image);
+            usernameTextView = itemView.findViewById(R.id.username_text);
+            contentTextView = itemView.findViewById(R.id.content_text);
+            datetimeTextView = itemView.findViewById(R.id.datetime_text);
+            likeButton = itemView.findViewById(R.id.like_button);
+            dislikeButton = itemView.findViewById(R.id.dislike_button);
         }
     }
 }
+
