a5930b2bf873934c2b79081fac61823373c41d8a
RaduGabrielMaría <rgabriel@ginzo.tech>
2023-05-30 13:01:50 +0200

    Image search

--- app/src/main/java/com/example/myapplication/AdaptadorPerfil.java
+++ app/src/main/java/com/example/myapplication/AdaptadorPerfil.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import static android.content.ContentValues.TAG;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,14 +23,25 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.List;
 
 public class AdaptadorPerfil extends RecyclerView.Adapter<AdaptadorPerfil.ViewHolder> {
     private List<PerfilSearch> perfilSearchList;
     private OnItemClickListener clickListener;
+    private FirebaseFirestore db;
+    Context context;
 
     public AdaptadorPerfil(List<PerfilSearch> PerfilSearch) {
         perfilSearchList = PerfilSearch;
+        db = FirebaseFirestore.getInstance();
     }
     public interface OnItemClickListener {
         void onItemClick(int position);
@@ -39,7 +53,7 @@ public class AdaptadorPerfil extends RecyclerView.Adapter<AdaptadorPerfil.ViewHo
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+         context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
 
         // Inflate the custom layout
@@ -59,7 +73,33 @@ public class AdaptadorPerfil extends RecyclerView.Adapter<AdaptadorPerfil.ViewHo
         final int itemPosition = position;
         PerfilSearch perfilSearch = perfilSearchList.get(position);
         holder.usernameTextView.setText(perfilSearch.getUsername() != null ? perfilSearch.getUsername().toString() : "");
-       //holder.profileImageView.setImageResource(perfilSearch.getPhoto() != null ? perfilSearch.getPhoto().toString() : "");
+        Query query = db.collection("users").whereEqualTo("Username", perfilSearch.getUsername());
+        query.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot querySnapshot) {
+                if (!querySnapshot.isEmpty()) {
+                    DocumentSnapshot documentSnapshot = querySnapshot.getDocuments().get(0);
+                    String photoUrl = documentSnapshot.getString("photo");
+                    Log.d(TAG, "photoUrl: " + photoUrl);
+                    if (photoUrl != null) {
+                        Glide.with(context)
+                                .load(photoUrl)
+                                .into(holder.profileImageView);
+                    } else {
+                        Glide.with(holder.itemView.getContext())
+                                .load(R.drawable.profile)
+                                .into(holder.profileImageView);
+                    }
+                } else {
+                    // El usuario no fue encontrado en la base de datos.
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, "Error getting user document", e);
+            }
+        });
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
